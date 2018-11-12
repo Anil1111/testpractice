3977d26ff77d824756894c885754efead6ddb166
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-12 20:53:28 +0900

    Mod rename class and method

--- TestNinja.UnitTests/UnitTest1.cs
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -4,9 +4,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TestNinja.UnitTests {
     // TestClass and TestMethod attributes belongs to MSTestFramework.
     [TestClass]
-    public class UnitTest1 {
+    public class ReservationTests {
         [TestMethod]
-        public void TestMethod1() {
+        public void CanBeCancelledBy_Scenario_ExpectedBehavior() {
         }
     }
 }
