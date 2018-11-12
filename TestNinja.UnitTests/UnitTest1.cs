40663ab2721392049bc4cd9e066eaa869ae520a6
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-12 21:03:38 +0900

    Add tripple A codes

--- TestNinja.UnitTests/UnitTest1.cs
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests {
     // TestClass and TestMethod attributes belongs to MSTestFramework.
@@ -7,10 +8,18 @@ namespace TestNinja.UnitTests {
     public class ReservationTests {
         [TestMethod]
         // 1) Specifies the name of the method on the test_
-        // 2) Scenario of our testing_
+        // 2) Scenario of the testing_
         // 3) Expected behavior
         public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() {
+            /* Tripple A convention */
+            // 1. Arrange: Initialize the objects
+            var reservation = new Reservation();
 
+            // 2. Act: Act on this object; call a method which is going to be tested
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // 3. Assert: Verify the result is correct
+            Assert.AreEqual(result, true);
         }
     }
 }
