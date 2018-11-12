4cdc1a436a8fecbc450ad4db76788520796bc93a
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-12 20:51:36 +0900

    Add first test stub

---
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests {
+    // TestClass and TestMethod attributes belongs to MSTestFramework.
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
