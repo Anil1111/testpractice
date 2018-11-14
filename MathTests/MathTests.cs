133eac33e906bd90eab69081b7e57ffc931343f2
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 15:05:58 +0900

    Add max tests

--- MathTests/MathTests.cs
+++ MathTests/MathTests.cs
@@ -16,5 +16,27 @@ namespace MathTests {
             // 3. Assert
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument() {
+            // In each test, you should start with a fresh, clean state; new object!
+            var m = new Math();
+            var result = m.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument() {
+            var m = new Math();
+            var result = m.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsTheSameArgument() {
+            var m = new Math();
+            var result = m.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
