5b0b1fb97d388306bb155d317bb7366341f23291
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 19:22:10 +0900

    Add testing void method

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests {
+    /// <summary>
+    /// Summary description for ErrorLoggerTests
+    /// </summary>
+    [TestFixture]
+    public class ErrorLoggerTests {
+
+        private ErrorLogger logger;
+
+        [SetUp]
+        public void SetUp() {
+            logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty() {
+
+            // This is "Command" type of function not "Query".
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
