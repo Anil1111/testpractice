cbea945520481263d4471b5277906b2f0732d3b9
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 14:56:56 +0900

    Add comments

--- MathTests/MathTests.cs
+++ MathTests/MathTests.cs
@@ -5,10 +5,15 @@ namespace MathTests {
     [TestFixture]
     public class MathTests {
         [Test]
-        public void Add_AllPositiveValues_ReturnsAPlusB() {
+        public void Add_WhenCalled_ReturnTheSumOfArguments() {
+            /* Tripple A convention */
+            // 1. Arrage
             var m = new Math();
 
+            // 2. Act
             var result = m.Add(1, 2);
+
+            // 3. Assert
             Assert.That(result, Is.EqualTo(3));
         }
     }
