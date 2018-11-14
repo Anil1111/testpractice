78da22cc97ee8df2198ad0180c542659b6e7d557
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 19:05:21 +0900

    Refactor align codes

--- TestNinja/Fundamentals/CustomerController.cs
+++ TestNinja/Fundamentals/CustomerController.cs
@@ -1,19 +1,17 @@
-﻿namespace TestNinja.Fundamentals
-{
-    public class CustomerController
-    {
-        public ActionResult GetCustomer(int id)
-        {
-            if (id == 0)
+﻿namespace TestNinja.Fundamentals {
+    public class CustomerController {
+        public ActionResult GetCustomer(int id) {
+            if (id == 0) {
                 return new NotFound();
-            
+            }
+
             return new Ok();
-        }        
+        }
     }
-    
+
     public class ActionResult { }
-    
+
     public class NotFound : ActionResult { }
- 
+
     public class Ok : ActionResult { }
 }
