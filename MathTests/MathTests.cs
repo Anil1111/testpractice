26580f06eef67eda0aae3b038910de9d602dc18d
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 14:53:54 +0900

    Add my first test method

---
+++ MathTests/MathTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace MathTests {
+    [TestFixture]
+    public class MathTests {
+        [Test]
+        public void Add_AllPositiveValues_ReturnsAPlusB() {
+            var m = new Math();
+
+            var result = m.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
---
+++ MathTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9ae8db3-1744-4717-ae35-6aa81aaec90b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
