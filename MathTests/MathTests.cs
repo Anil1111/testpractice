f28b4c98131029a41c3371b43c239622f5800f02
Sungkuk Park <sungkukshawnpark@gmail.com>
2018-11-14 16:57:52 +0900

    Mod comment out disabling codes

--- MathTests/MathTests.cs
+++ MathTests/MathTests.cs
@@ -18,7 +18,7 @@ namespace MathTests {
 
         [Test]
         // Disabling the test is the better approach than commenting it out or deleting it.
-        [Ignore("Because I wanted to!")]
+        //[Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments() {
             /* Tripple A convention */
             // 1. Arrage
